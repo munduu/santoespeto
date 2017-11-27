9c27141d82da63c2c544ada582edbd65d8004599
mateusmaia <mateusmpontes@gmail.com>
2017-11-27 10:20:39 -0200

    1.5 Santo Espeto

--- www/js/index_user_scripts.js
+++ www/js/index_user_scripts.js
@@ -81,16 +81,17 @@
             },
             success:function(resultado){
                 $('.loader').hide(); 
-				alert (resultado);
-                if(resultado == '1'){
+				alert(resultado);
+                if(resultado == 1){
+					alert(resultado);
                     alert('Mesa Aberta !');
 					//$('.btn_mesa').click();
                     entrar_mesa();
-                }else if(resultado == '2'){
+                }else if(resultado == 2){
 					alert('Campo Obrigatorio Vazio: NÚMERO MESA VAZIO!');
-				}else if(resultado == '3'){
+				}else if(resultado == 3){
 					alert('Campo Obrigatorio Vazio: EVENTO NÃO SELECIONADOS, CONTATE A ADMINISTRAÇÃO.');
-				}else if(resultado == '4'){
+				}else if(resultado == 4){
 					alert('MESA *** '+n_mesa+' *** JA ESTA ABERTA!');
 				}	
             },
