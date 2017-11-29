77ea8dd46d457b981a6775abc7aec598267e0d7b
mateusmaia <mateusmpontes@gmail.com>
2017-11-29 14:57:27 -0200

    2.23

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 //INOVA
 //var url_geral ="http://192.168.1.100:8080/igestao/app/santoespeto_php/";
 
-var url_geral ="http://192.168.1.180:8080/igestao/app/santoespeto_php/";
-
 //INTERNO
 //var url_geral ="http://192.168.0.2:1020/app/";
+
+var url_geral = "http://192.168.0.10:8080/igestao/app/santoespeto_php/";
